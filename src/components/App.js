38f2eb1f43c8f692d5fb0028c15e3de2797e169f
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-09-01 17:05:52 -0700

    Limit maximum number of mines to number of spaces on board

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,14 @@ class App extends React.Component {
       }
     }
     render(){
-
+      let maxMines = this.state.boardSize **2
        return (
         <div className="App">
           <h1>Minesweeper</h1>
           <label for='boardSize'>Board Size </label>
-          <input onChange ={()=>{this.setState({boardSize: document.getElementById('boardSize').value})}} placeholder = '5' id= 'boardSize' type= 'number' min='5' max='30'></input>
+          <input onChange ={()=>{this.setState({boardSize: document.getElementById('boardSize').value})}} placeholder = '5' id= 'boardSize' type= 'number' min='5' max='20'></input>
           <label for='numOfMines'>Number of Mines</label>
-          <input onChange ={()=>{this.setState({numOfMines: document.getElementById('numOfMines').value})}} placeholder = '1' id="numOfMines" type= 'number' min='1' max='400'></input>
+          <input onChange ={()=>{this.setState({numOfMines: document.getElementById('numOfMines').value})}} placeholder = '1' id="numOfMines" type= 'number' min='1' max={maxMines}></input>
           <Board numOfMines = {this.state.numOfMines } boardSize = {this.state.boardSize}/>
         </div>
       );
